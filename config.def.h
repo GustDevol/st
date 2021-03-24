fa65a764f4ff988000e4cd0ecc0de57417a0c1f0
gourish13 <sadhu5gourish@outlook.com>
2021-03-24 18:15:11 +0530

    added Harfbuzz and patched ligatures👍🏻👍🏻👍🏻

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:size=12:antialias=true:autohint=true";
+static char *font = "Fira Code Medium:size=12:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"Symbola:size=12:antialias=true:autohint=true",
